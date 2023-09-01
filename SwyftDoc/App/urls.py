5a6f01519d27faf1812e4791dd6e956f1bfe9a88
Kaushal Kumar <kkum9480@uni.sydney.edu.au>
2023-09-01 23:47:22 +1000

    SwyftDoc

--- SwyftDoc/App/urls.py
+++ SwyftDoc/App/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from App.views import *
 
 urlpatterns = [
-    path('', index)
+    path('', index),
+    path('upload_document', upload_document)
 ]
--- SwyftDoc/App/views.py
+++ SwyftDoc/App/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.files.storage import FileSystemStorage
+from PyPDF2 import PdfReader
+
+from cryptography.fernet import Fernet
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Welcome")
+    return render(request, 'index.html')
+
+def upload_document(request):
+    file = request.FILES['file']
+    fs = FileSystemStorage()
+    filename = fs.save(file.name, file)
+    document = PdfReader(filename)
+    text = document.read().decode('utf-8')
+
+    print(text)
+    # signature = Fernet.generate_key().encode('utf-8')
+    # encrypted_text = Fernet(signature).encrypt(text.encode('utf-8'))
+    # return HttpResponse(encrypted_text)
