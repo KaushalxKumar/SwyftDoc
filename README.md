9d08b2e79d609ae34b64f091edd606b7dcd342e6
Kaushal Kumar <kkum9480@uni.sydney.edu.au>
2023-11-08 18:53:18 +1100

    READ Me & Remove Personal Credentials

--- README.md
+++ README.md
@@ -1,1 +1,66 @@
+# The University of Sydney
 # SwyftDoc - Thesis Project 
+**Kaushal Kumar (kkum9480)** 
+
+**480236025**
+
+## About
+SwyftDoc is a quantum resistant blockchain-based document certification and verification platform.
+
+## How To Install & Run (Windows)
+
+### Project Components 
+Ensure the following applications is installed on your local machine.
+* Python - [Python Installation Guide](https://wiki.python.org/moin/BeginnersGuide/Download)
+* Truffle - [Truffle Installation Guide](https://trufflesuite.com/docs/truffle/how-to/install/)
+* Ganache - [Ganache Installation Guide](https://trufflesuite.com/ganache/)
+* IDE - [PyCharm Recommended](https://www.jetbrains.com/pycharm/download/?section=windows)
+
+### Clone Project
+```angular2html
+$ git clone https://github.com/KaushalxKumar/SwyftDoc.git
+```
+
+### 1. Install Django and Python Modules 
+Navigate into the inner "SwyftDoc" folder (SwyftDoc\SwyftDoc).
+```angular2html
+$ pip install -r requirements.txt
+```
+
+### 2. Ganache Blockchain
+1. Open up the ganache application.
+2. Create new workspace and chose and appropriate name.
+3. Start Ganache blockchain.
+
+### 3. Deploy Smart Contract
+Navigate into the "Truffle" folder (SwyftDoc\Truffle).
+Ensure "Host" and "Port" in the "truffle-config.js" file is the same as your Ganache Blockchain.
+
+```angular2html
+$ truffle migrate --network development
+```
+
+### 4. SendGrid 
+1. Create [SendGrid](https://sendgrid.com/) account. 
+2. In Settings > API Keys. Create a new API save the key itself.
+3. In Settings > Sender Authentication. Verify the ownership of a single email address that you own.
+ 
+### 5. Input Credentials
+Navigate into the inner "SwyftDoc" folder (SwyftDoc\SwyftDoc).
+
+1. Open up "credentials.py" and replace the required fields with your own credentials.
+
+If not using the default Ganache URL.
+2. Change "ganache_url" in both "certify.py" and "verify.py" files in the Interactions folder (SwyftDoc\SwyftDoc\Interactions) to the URL of your Ganache blockchain.
+
+### 6. Run
+Ensure Ganache Blockchain is open and running.
+
+Navigate into the inner "SwyftDoc" folder (SwyftDoc\SwyftDoc).
+
+```angular2html
+python manage.py runserver 
+```
+
+Open URL in browser
+[SwyftDoc](http://127.0.0.1:8000)
--- SwyftDoc/credentials.py
+++ SwyftDoc/credentials.py
@@ -1,10 +1,10 @@
-SENDGRID_API_KEY = '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'
-FROM_EMAIL = 'kkum9480@uni.sydney.edu.au'
+SENDGRID_API_KEY = 'REPLACE WITH SENDGRID API KEY'
+FROM_EMAIL = 'REPLACE WITH VERIFIED SENDGRID EMAIL'
 BASE_URL = 'http://127.0.0.1:8000'
 
-CERTIFY_CONTRACT_ADDRESS = '0x917EF4417489180621c5220DE2B87A3eabFa9E65'
+CERTIFY_CONTRACT_ADDRESS = 'REPLACE WITH DEPLOYED CERTIFY CONTRACT ADDRESS FROM YOUR GANACHE BLOCKCHAIN'
 CERTIFY_CONTRACT_ABI = '[{"anonymous": false,"inputs": [{"indexed": true,"internalType": "address","name": "owner","type": "address"},{"indexed": true,"internalType": "bytes32","name": "hash","type": "bytes32"}],"name": "DocumentAdded","type": "event"},{"inputs": [],"name": "documentCount","outputs": [{"internalType": "uint256","name": "","type": "uint256"}],"stateMutability": "view","type": "function","constant": true},{"inputs": [{"internalType": "bytes32","name": "","type": "bytes32"}],"name": "documents","outputs": [{"internalType": "address","name": "owner","type": "address"},{"internalType": "bytes","name": "signature","type": "bytes"},{"internalType": "bytes","name": "publicKey","type": "bytes"},{"internalType": "bytes32","name": "hash","type": "bytes32"}],"stateMutability": "view","type": "function","constant": true},{"inputs": [{"internalType": "bytes","name": "_signature","type": "bytes"},{"internalType": "bytes","name": "_publicKey","type": "bytes"},{"internalType": "bytes32","name": "_hash","type": "bytes32"}],"name": "addDocument","outputs": [],"stateMutability": "nonpayable","type": "function"}]'
-ETHEREUM_ADDRESS = '0x7aF240B73746B78F334dF38cBc9f02F94a6E378C'
+ETHEREUM_ADDRESS = 'REPLACE WITH AN ETHEREUM ADDRESS FROM YOUR GANACHE BLOCKCHAIN'
 
-VERIFY_CONTRACT_ADDRESS = '0xfA2C3AA8D8BcC0156c6428eEcA32205733328eA0'
+VERIFY_CONTRACT_ADDRESS = 'REPLACE WITH DEPLOYED VERIFY CONTRACT ADDRESS FROM YOUR GANACHE BLOCKCHAIN'
 VERIFY_CONTRACT_ABI = '[{"inputs":[{"internalType":"address","name":"_certifyContractAddress","type":"address"}],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[],"name":"certifyContract","outputs":[{"internalType":"contract Certify","name":"","type":"address"}],"stateMutability":"view","type":"function","constant":true},{"inputs":[{"internalType":"bytes32","name":"_hash","type":"bytes32"}],"name":"verifyDocument","outputs":[{"internalType":"address","name":"","type":"address"},{"internalType":"bytes","name":"","type":"bytes"},{"internalType":"bytes","name":"","type":"bytes"}],"stateMutability":"view","type":"function","constant":true}]'
